0ba15489986786262a23d6f33d2449c48062c067
Briskybee(Belle) <serdena.ss@obmontessori.edu.ph>
2026-09-04 11:35:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+#Serdeña, Sy
+studname = "Kaizen Sy" #String
+studage = 15 #Integer
+studheight = 160.02 #float
+countries_visit = ["France", "Switzerland", "Korea"]#list
+student_type = False #boolean
+stud_pref = {
+  "color": "red",
+  "car brand":"bentley",
+  "shoe size" : 9,
+  "bff" : "r2 & jack"
+ }
+stud_fruits = {
+    "Banana", 
+    "Strawberry", 
+    "Mango",
+    "Orange",
+    "Apple"
+ }
+
+days_week = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+)
+
+print("Name:", studname)
+print("Age:", studage)
+print("height:", studheight)
+print("Countries Visited:", countries_visit)
+print("Student Type:", student_type)
+print("Student Preferences:", stud_pref)
+print("Student Fruits:", stud_fruits)
+print("Days of the Week:", days_week)
+
+
+
+
+
+
+
+
